e47bc33357363919887ffb6db9e9b5e660fc5964
Victor <victorbdesign@gmail.com>
2023-04-04 23:38:29 +0200

    ch

--- pages/services.js
+++ pages/services.js
@@ -48,7 +48,7 @@ export default function Services() {
                 <h3 className="text-4xl font-bold mb-5">
                   Full-stack development
                 </h3>
-                <p className="text-xl text-gold-500">
+                <p style={{display:"none"}} className="text-xl text-gold-500">
                   Only 80€/hour
                 </p>
               </div>
@@ -177,7 +177,7 @@ export default function Services() {
                 <h3 className="text-4xl font-bold mb-5">
                   Front-end development
                 </h3>
-                <p className="text-xl text-gold-500">
+                <p style={{display:"none"}} className="text-xl text-gold-500">
                  Only 70€/hour
                 </p>
               </div>
